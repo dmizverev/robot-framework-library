cc9d2bf5d9de27eb6afd241857ea01a7f80ebc93
Dmitriy.Zverev <Dmitriy.Zverev@billing.ru>
2014-05-20 15:11:03 +0400

    Update TestRailListener
    * add support for TestRail fields: defects, references
    * test results added to TestRail in method "close" - one api call instead each test

--- listener/TestRailListener.py
+++ listener/TestRailListener.py
@@ -4,6 +4,7 @@ import re
 import json
 from robot.api import logger
 from testrail import APIClient
+# from idlelib.rpc import _getattributes
 
 __author__ = "Dmitriy.Zverev"
 __license__ = "Apache License, Version 2.0"
@@ -24,13 +25,13 @@ class TestRailListener(object):
     | *** Test Cases ***
     | Autotest name
     |    [Documentation]    Autotest documentation
-    |    [Tags]    testrailid=10
+    |    [Tags]    testrailid=10    defects=BUG-1, BUG-2    references=REF-3, REF-4
     |    Fail    Test fail message
     4. Run Robot Framework with listener:\n
     | set ROBOT_SYSLOG_FILE=syslog.txt
     | pybot.bat --listener TestRailListener.py:testrail_server_name:tester_user_name:tester_user_password:20:update  autotest.txt
-    5. Test with case_id=10 will be marked as failed in TestRail with message "Test fail message". 
-    Also title and description of this test will be updated in TestRail. Parameter "update" is optional.
+    5. Test with case_id=10 will be marked as failed in TestRail with message "Test fail message" and defects "BUG-1, BUG-2".
+    Also title, description and references of this test will be updated in TestRail. Parameter "update" is optional.
     """
 
     ROBOT_LISTENER_API_VERSION = 2
@@ -51,6 +52,7 @@ class TestRailListener(object):
         self.client.password = password
         self.run_id = run_id
         self.update = update
+        self.results = list()
         logger.info('[TestRailListener] url: ' + testrail_url)
         logger.info('[TestRailListener] user: ' + user)
         logger.info('[TestRailListener] password: ' + password)
@@ -58,57 +60,57 @@ class TestRailListener(object):
 
     def end_test(self, name, attrs):
         """
-        Save test result in TestRail
+        Update test case in TestRail
         
         *Args:*\n
         _name_ - the name of test case in Robot Framework\n
         _attrs_ - attributes of test case in Robot Framework
         """
 
-        case_id = self._getTestCaseIDFromTestTags (attrs['tags'])
+        tags_value = self._getTagsValue (attrs['tags'])
+        case_id = tags_value['testrailid']
+        defects = tags_value['defects']
+        references = tags_value['references']
         if attrs['status'] == 'PASS':
             status_id = 1
         else :
             status_id = 5
         if case_id:
+            # Add results to list
+            test_result = {'case_id': case_id, 'status_id': status_id, 'comment': attrs['message'], 'defects': defects}
+            self.results.append(test_result)
+
             # Update test case
             if self.update:
                 logger.info ('[TestRailListener] update of test ' + case_id + ' in TestRail')
                 description = attrs['doc'] + '\n' + 'Path to test: ' + attrs['longname']
                 result = self.client.send_post(
                                                'update_case/' + case_id,
-                                               { 'title': name, 'type_id': 1, 'custom_case_description': description}
+                                               { 'title': name, 'type_id': 1, 'custom_case_description': description, 'refs': references}
                 )
                 logger.info ('[TestRailListener] result for method update_case ' + json.dumps(result, sort_keys = True, indent = 4))
-            # Set test result
-            logger.info ('[TestRailListener] report result of test ' + case_id + ' to TestRail')
-            result = self.client.send_post(
-                                           'add_result_for_case/' + self.run_id + '/' + case_id,
-                                           { 'status_id': status_id, 'comment': attrs['message'] }
-            )
-            logger.info ('[TestRailListener] result for method add_result_for_case ' + json.dumps(result, sort_keys = True, indent = 4))
 
-
-    def _getTestCaseIDFromTestTags(self, tags):
-        """ 
-        Getting the TestRail's case_id from the tag that should exist in the autotest.
-        
-        *Args:*\n
-        _tags_ - list of tags in autotest;\n
-        
-        *Returns:*\n
-        case_id for TestRail
-        
-        *Example:*\n
-        [Tags]    testrailid=10\n
-        =>\n
-        case_id = 40
+    def close (self):
+        """
+        Save test results for all tests in TestRail
         """
 
-        match_tag = 'testrailid'
-        for tag in tags:
-            match = re.match(match_tag, tag)
-            if match :
-                split_tag = tag.split('=')
-                testcaseID = split_tag[1]
-                return testcaseID
+        self.client.send_post('add_results_for_cases/' + self.run_id, { 'results':self.results })
+
+    def _getTagsValue (self, tags):
+        """
+        Get value from robot framework's tags for TestRail.
+        """
+        attributes = dict()
+        matchers = ['testrailid', 'defects', 'references']
+        for matcher in matchers :
+            for tag in tags:
+                match = re.match(matcher, tag)
+                if match :
+                    split_tag = tag.split('=')
+                    tag_value = split_tag[1]
+                    attributes [matcher] = tag_value
+                    break
+                else:
+                    attributes [matcher] = None
+        return attributes
