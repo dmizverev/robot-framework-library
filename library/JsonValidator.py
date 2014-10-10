66078eaefcc7868b657f1c66ea5738c985c10654
Dmitriy.Zverev <Dmitriy.Zverev@billing.ru>
2014-10-10 12:07:23 +0400

    Change JsonValidator library: add pretty_print keyword

--- library/JsonValidator.py
+++ library/JsonValidator.py
@@ -63,16 +63,16 @@ class JsonValidator(object):
         try:
             jsonschema.validate(checked_json, schema)
         except jsonschema.ValidationError , e:
-            raise AssertionError ('Json validation error: ' + e.message)
+            raise AssertionError ('Json validation error. Element: %s. Error: %s. ' % (e.path[0], e.message))
         except jsonschema.SchemaError , e:
             raise AssertionError ('Json-schema error:' + e.message)
 
-    def validate_jsonschema_from_file (self, input_json, path_to_schema):
+    def validate_jsonschema_from_file (self, json_string, path_to_schema):
         """
-        Проверка json по схеме, загруженной из файла.
+        Проверка json по схеме, загружаемой из файла.
         
         *Args:*\n
-        _input_json_ - json-строка; в дальнейшем сериализуется при помощи json.loads;\n
+        _json_string_ - json-строка; в дальнейшем сериализуется при помощи json.loads;\n
         _path_to_schema_ - путь к файлу со схемой json;
         
         *Raises:*\n
@@ -86,7 +86,7 @@ class JsonValidator(object):
         """
 
         schema = open(path_to_schema).read()
-        load_input_json = self._parse_json (input_json)
+        load_input_json = self._parse_json (json_string)
 
         try:
             load_schema = json.loads(schema)
@@ -95,12 +95,12 @@ class JsonValidator(object):
 
         self._validate_json (load_input_json, load_schema)
 
-    def validate_jsonschema (self, input_json, input_schema):
+    def validate_jsonschema (self, json_string, input_schema):
         """
         Проверка json по схеме.
         
         *Args:*\n
-        _input_json_ - json-строка; в дальнейшем сериализуется при помощи json.loads;\n
+        _json_string_ - json-строка; в дальнейшем сериализуется при помощи json.loads;\n
         _input_schema_ - схема в виде строки; в дальнейшем сериализуется при помощи json.loads.
         
         *Raises:*\n
@@ -114,7 +114,8 @@ class JsonValidator(object):
         | Simple | ${schema}=   | OperatingSystem.Get File |   ${CURDIR}${/}schema_valid.json |
         |  | Validate jsonschema  |  {"foo":bar}  |  ${schema} |
         """
-        load_input_json = self._parse_json (input_json)
+        
+        load_input_json = self._parse_json (json_string)
 
         try:
             load_schema = json.loads(input_schema)
@@ -124,18 +125,19 @@ class JsonValidator(object):
         self._validate_json (load_input_json, load_schema)
 
     def _parse_json (self, source):
+        #source = source.decode('ascii', 'ignore')
         try:
             load_input_json = json.loads(source)
         except ValueError, e:
             raise ValueError("Could not parse '%s' as JSON: %s" % (source, e))
         return    load_input_json
 
-    def get_elements (self, input_json, expr, result_type = 'VALUE', debug = 0, use_eval = True):
+    def get_elements (self, json_string, expr, result_type = 'VALUE', debug = 0, use_eval = True):
         """
-        Возвращает список элементов из _input_json_, соответствующих jsonpath выражению.
+        Возвращает список элементов из _json_string_, соответствующих jsonpath выражению.
         
         *Args:*\n
-        _input_json_ - json-строка; в дальнейшем сериализуется при помощи json.loads;\n
+        _json_string_ - json-строка; в дальнейшем сериализуется при помощи json.loads;\n
         _expr_ - jsonpath выражение;\n
         _result_type_ - тип возвращаемого значения; принимает значения VALUE - возвращается список из json-строк, 
                         IPATH - возвращается список значений, значение по умолчанию: VALUE\n
@@ -160,19 +162,19 @@ class JsonValidator(object):
         {u'category': u'reference', u'price': 8.95, u'title': u'Sayings of the Century', u'author': u'Nigel Rees'}
         """
 
-        load_input_json = self._parse_json (input_json)
+        load_input_json = self._parse_json (json_string)
         value = jsonpath.jsonpath (load_input_json, expr, result_type, int(debug), use_eval)
 
         if not value:
             raise AssertionError ('Could not get elements by jsonpath expression %s' % expr)
         return    value
 
-    def element_should_exist (self, input_json, expr, debug = 0, use_eval = True):
+    def element_should_exist (self, json_string, expr, debug = 0, use_eval = True):
         """
         Проверка существования одного или более элементов, соответствующих jsonpath выражению.
         
         *Args:*\n
-        _input_json_ - json-строка; в дальнейшем сериализуется при помощи json.loads;\n
+        _json_string_ - json-строка; в дальнейшем сериализуется при помощи json.loads;\n
         _expr_ - jsonpath выражение;\n
         _debug_ - выводить отладочную информацию, принимает значения 0 или 1;\n
         _use_eval_ - вычисление значений в _expr_.
@@ -190,11 +192,11 @@ class JsonValidator(object):
         """
 
         try:
-            self.get_elements (input_json, expr, debug = debug, use_eval = use_eval)
+            self.get_elements (json_string, expr, debug = debug, use_eval = use_eval)
         except Exception:
             raise AssertionError ('Elements %s does not exist' % expr)
 
-    def element_should_not_exist (self, input_json, expr, debug = 0, use_eval = True):
+    def element_should_not_exist (self, json_string, expr, debug = 0, use_eval = True):
         """
         Проверка отсутствия одного или более элементов, соответствующих jsonpath выражению.
         
@@ -202,7 +204,40 @@ class JsonValidator(object):
         """
 
         try:
-            self.get_elements (input_json, expr, debug = debug, use_eval = use_eval)
+            self.get_elements (json_string, expr, debug = debug, use_eval = use_eval)
         except Exception:
             return
         raise AssertionError ('Elements %s exist but should not' % expr)
+
+    def pretty_print_json (self, json_string):
+        """
+        Возврещает отформатированную json-строку _json_string_.\n
+        Используется метод json.dumps с настройкой _indent=2, ensure_ascii=False_.
+        
+        *Args:*\n
+        _json_string_ - json-строка.
+        
+        *Example:*\n
+        | *Settings* | *Value* |
+        | Library    | JsonValidator |
+        | Library    | OperatingSystem |
+        | *Test Cases* | *Action* | *Argument* | *Argument* |
+        | Check element | ${pretty_json}=   | Pretty print json |   {a:1,foo:[{b:2,c:3},{d:"baz",e:4}]} |
+        | | Log  |  ${pretty_json}  |
+        =>\n
+        | {
+        |    "a": 1, 
+        |    "foo": [
+        |      {
+        |        "c": 3, 
+        |        "b": 2
+        |      }, 
+        |      {
+        |        "e": 4, 
+        |        "d": "baz"
+        |      }
+        |    ]
+        | }
+        """
+        
+        return json.dumps(self._parse_json(json_string), indent=2, ensure_ascii=False)
